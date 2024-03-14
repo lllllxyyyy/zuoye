f5d387dfe436854b5747b9122ba03305a889ee75
lllllxyyyy <161725594+lllllxyyyy@users.noreply.github.com>
2024-03-14 21:14:52 +0800

    3

---
+++ 未命名8.js
@@ -0,0 +1,10 @@
+function sayHello(){
+  var response = promrt("What is your name?");
+  alert('Hello' + response + ', have a nice day!');
+}
+//sayHello();
+function showTopic(){
+  var x = document.getElementBYId('demo');
+  x.style.fonSize = '25px';
+  x.style.color ='red';
+}
